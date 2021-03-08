a2cb83cac13c01a9df765cff9a31bf335e270e24
sybilal <sbilalahmed95@gmail.com>
2021-03-08 13:19:32 +0400

    lecture 13 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
